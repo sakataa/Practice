dfd0cbf091e336e895b218186e12e00c6d9fcf10
Walter Tran <walter.tran@nexcel.vn>
2016-02-04 16:22:56 +0700

    #Bootstrap - Finish creating responsive design

--- BootstrapTraining/Exercise5/Walter/js/page.js
+++ BootstrapTraining/Exercise5/Walter/js/page.js
@@ -3,5 +3,7 @@ $(document).ready(function(){
 		var leftPanel = $('#left-panel');
 		var headerLeft = $('#header-left');
 		var rightPanel = $('.right-side');
+
+		$('.wrapper').toggleClass('expand');
 	});
 });
