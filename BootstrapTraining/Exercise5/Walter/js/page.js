c21980f6b76624a2c2e3c0b5142a1c7f61376255
Walter Tran <walter.tran@nexcel.vn>
2016-02-04 14:41:40 +0700

    #Bootstrap - Create responsive without javascript

---
+++ BootstrapTraining/Exercise5/Walter/js/page.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('#menu-toggle').click(function(){
+		var leftPanel = $('#left-panel');
+		var headerLeft = $('#header-left');
+		var rightPanel = $('.right-side');
+	});
+});
