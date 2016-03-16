23407b25808cf35b550d0b907c14a9fc2a9728ba
sakataa <tientm.ctt@gmail.com>
2016-03-17 00:31:59 +0700

    #Bootstrap - Web Team - Ex1 - Create banner

--- WebTeam-WebDesignTraining/2/Walter/scripts/site.js
+++ WebTeam-WebDesignTraining/2/Walter/scripts/site.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
   $("#owl-carousel").owlCarousel({
   		pagination: false,
   		slideSpeed: 500,
-  		items : 2,
-      	itemsDesktop : [1199,2],
-      	itemsDesktopSmall : [979,2]
+  		items : 1,
+      	itemsDesktop : [1199,1],
+      	itemsDesktopSmall : [979,1]
   });
 
   $('#next').click(function () {
