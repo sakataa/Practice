9187f222c1d359a93bfba5e3c2aed4dd9e3c7979
Walter Tran <walter.tran@nexcel.vn>
2016-03-21 10:25:41 +0700

    #Bootstrap - Web Team - Ex1 - Set owl-carousel for about team block

--- WebTeam-WebDesignTraining/2/Walter/scripts/site.js
+++ WebTeam-WebDesignTraining/2/Walter/scripts/site.js
@@ -15,5 +15,21 @@ $(document).ready(function() {
 	$('#prev').click(function () {
     	$('#owl-carousel').trigger('owl.prev');
 	});
+
+  $("#owl-carousel-aboutTeam").owlCarousel({
+      pagination: false,
+      slideSpeed: 500,
+      itemsDesktop : [2560,1],
+      itemsDesktopSmall : [979,1],
+      itemsTablet: [768, 1]
+  });
+
+  $('#photoNext').click(function () {
+      $('#owl-carousel-aboutTeam').trigger('owl.next');
+  });
+
+  $('#photoPrev').click(function () {
+      $('#owl-carousel-aboutTeam').trigger('owl.prev');
+  });
  
 });
