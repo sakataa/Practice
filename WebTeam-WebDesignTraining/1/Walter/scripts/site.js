1b33bd4ef658ce38ba72a726418d345cc71d1a7e
Walter Tran <walter.tran@nexcel.vn>
2016-03-22 16:17:21 +0700

    #Bootstrap - Web Team - Ex2 - Add Exercise2

--- WebTeam-WebDesignTraining/1/Walter/scripts/site.js
+++ WebTeam-WebDesignTraining/1/Walter/scripts/site.js
@@ -1,19 +1,38 @@
 $(document).ready(function() {
- 
-  $("#owl-carousel").owlCarousel({
-  		pagination: false,
-  		slideSpeed: 500,
-  		items : 2,
-      	itemsDesktop : [1199,2],
-      	itemsDesktopSmall : [979,2]
-  });
+  initCarouselPlugin("owl-carousel", "prev", "next");
+  initCarouselPlugin("owl-carousel-aboutTeam", "photoPrev", "photoNext");
+  initEvent();
+  
 
-  $('#next').click(function () {
-    	$('#owl-carousel').trigger('owl.next');
-	});
+  function initCarouselPlugin(idElement, idPrev, idNext){
+    $('#' + idElement).owlCarousel({
+        pagination: false,
+        slideSpeed: 500,
+        itemsDesktop : [2560,1],
+        itemsDesktopSmall : [979,1],
+        itemsTablet: [768, 1]
+    });
 
-	$('#prev').click(function () {
-    	$('#owl-carousel').trigger('owl.prev');
-	});
+    $('#' + idPrev).click(function () {
+        $('#' + idElement).trigger('owl.prev');
+    });
+
+    $('#' + idNext).click(function () {
+        $('#' + idElement).trigger('owl.next');
+    });   
+  }
+
+  function initEvent(){
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();    
+        if (scroll > 60) {
+            $("#headerNavbar").addClass("fixed-top");
+        }
+        else {
+             $("#headerNavbar").removeClass("fixed-top");
+        }
+            
+    });
+  }
  
 });
