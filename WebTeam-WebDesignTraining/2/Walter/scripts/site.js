c56c18fba0693d9867c5b40b6587415366c7c7fc
sakataa <tientm.ctt@gmail.com>
2016-04-06 23:42:40 +0700

    #Bootstrap - Web Team - Ex2 - Make carousel for product

--- WebTeam-WebDesignTraining/2/Walter/scripts/site.js
+++ WebTeam-WebDesignTraining/2/Walter/scripts/site.js
@@ -22,4 +22,26 @@
 		$(window).scrollTop(0);
 	});
 
+	var $productList = $('#productList');
+	registerSlick($productList, 4, "#prevMainProduct", "#nextMainProduct");
+
+	var $popularProducts = $('#popularProductList');
+	registerSlick($popularProducts, 3, "#prevPopularProduct", "#nextPopularProduct");
+
+	var $recentPost = $('#recentPost');
+	registerSlick($recentPost, 2, "#prevPost", "#nextPost");
+
+	var $ourBrandList = $('#ourBrandList');
+	registerSlick($ourBrandList, 6, "#prevBrand", "#nextBrand");
+
+	function registerSlick($element, slideToShow, idPrev, idNext){
+		$element.slick({
+		  	infinite: true,
+		  	slidesToShow: slideToShow,
+		  	slidesToScroll: 1,
+		  	prevArrow: idPrev,
+	        nextArrow: idNext
+		});
+	}
+
 })();
