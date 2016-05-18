050d15319273e9d04366b9b9a2831a1ce268c09d
sakataa <tientm.ctt@gmail.com>
2016-05-18 20:51:11 +0700

    Ex4 - Change icon when sub menu is expanded

--- WebTeam-WebDesignTraining/4/Walter/scripts/site.js
+++ WebTeam-WebDesignTraining/4/Walter/scripts/site.js
@@ -9,4 +9,17 @@
 	$("#topMoving").click(function(){
 		$(window).scrollTop(0);
 	});
+
+	$(".menu__item").click(function(){
+		var element = $(this);
+		var isActive = element.attr("aria-expanded") === "false";
+		
+		var iconExpand = element.find(".menu__item--icon-expand");
+		if(isActive === true){
+			iconExpand.removeClass("fa-angle-right").addClass("fa-angle-down");			
+		}
+		else{
+			iconExpand.removeClass("fa-angle-down").addClass("fa-angle-right");
+		}		
+	});
 })();
