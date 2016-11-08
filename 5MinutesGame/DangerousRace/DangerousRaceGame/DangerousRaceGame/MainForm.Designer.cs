97639992a4cce9af1b01ca8c90788b4846b70b9c
sakataa <tientm.ctt@gmail.com>
2016-11-08 23:30:17 +0700

    #0000 - Dangerous Race Game for 5 minutes - Create UI question list

--- 5MinutesGame/DangerousRace/DangerousRaceGame/DangerousRaceGame/MainForm.Designer.cs
+++ 5MinutesGame/DangerousRace/DangerousRaceGame/DangerousRaceGame/MainForm.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnRollDice = new System.Windows.Forms.Button();
@@ -36,11 +38,17 @@
             this.label3 = new System.Windows.Forms.Label();
             this.lblDice1 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.lblTotal = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -57,15 +65,35 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(841, 158);
+            this.panel1.Size = new System.Drawing.Size(1018, 139);
             this.panel1.TabIndex = 0;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(438, 31);
+            this.lblTotal.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(0, 22);
+            this.lblTotal.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(344, 31);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Total:";
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(580, 62);
+            this.pictureBox2.Location = new System.Drawing.Point(579, 31);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(128, 93);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -74,7 +102,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(94, 62);
+            this.pictureBox1.Location = new System.Drawing.Point(93, 31);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(128, 93);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -83,7 +111,7 @@
             // 
             // btnRollDice
             // 
-            this.btnRollDice.Location = new System.Drawing.Point(349, 116);
+            this.btnRollDice.Location = new System.Drawing.Point(348, 85);
             this.btnRollDice.Name = "btnRollDice";
             this.btnRollDice.Size = new System.Drawing.Size(103, 39);
             this.btnRollDice.TabIndex = 4;
@@ -94,7 +122,7 @@
             // lblDice2
             // 
             this.lblDice2.AutoSize = true;
-            this.lblDice2.Location = new System.Drawing.Point(708, 37);
+            this.lblDice2.Location = new System.Drawing.Point(707, 6);
             this.lblDice2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblDice2.Name = "lblDice2";
             this.lblDice2.Size = new System.Drawing.Size(0, 22);
@@ -103,7 +131,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(615, 37);
+            this.label3.Location = new System.Drawing.Point(614, 6);
             this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(66, 22);
@@ -113,7 +141,7 @@
             // lblDice1
             // 
             this.lblDice1.AutoSize = true;
-            this.lblDice1.Location = new System.Drawing.Point(222, 37);
+            this.lblDice1.Location = new System.Drawing.Point(221, 6);
             this.lblDice1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblDice1.Name = "lblDice1";
             this.lblDice1.Size = new System.Drawing.Size(0, 22);
@@ -122,37 +150,75 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(128, 37);
+            this.label1.Location = new System.Drawing.Point(127, 6);
             this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(66, 22);
             this.label1.TabIndex = 0;
             this.label1.Text = "Dice 1:";
             // 
-            // lblTotal
+            // panel2
             // 
-            this.lblTotal.AutoSize = true;
-            this.lblTotal.Location = new System.Drawing.Point(439, 62);
-            this.lblTotal.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(0, 22);
-            this.lblTotal.TabIndex = 8;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 139);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1018, 252);
+            this.panel2.TabIndex = 1;
             // 
-            // label4
+            // textBox1
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(345, 62);
-            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(56, 22);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Total:";
+            this.textBox1.Location = new System.Drawing.Point(93, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 97);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Team A:";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.textBox2);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 391);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1018, 236);
+            this.panel3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(94, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 27);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 104);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 22);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Team B:";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(841, 427);
+            this.ClientSize = new System.Drawing.Size(1018, 627);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5);
@@ -163,6 +229,10 @@
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -179,6 +249,12 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lblTotal;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- 5MinutesGame/DangerousRace/DangerousRaceGame/DangerousRaceGame/MainForm.cs
+++ 5MinutesGame/DangerousRace/DangerousRaceGame/DangerousRaceGame/MainForm.cs
@@ -1,19 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DangerousRaceGame
 {
-    using System.IO;
-    using System.Threading;
-    using Services;
-
     public partial class MainForm : Form
     {
         private readonly Dictionary<int, string> _dicePicture = new Dictionary<int, string>()
@@ -26,17 +18,25 @@ namespace DangerousRaceGame
             {6, "../../Images/6.png"}
         };
 
+        private readonly List<Label> _questionsForTeamA;
+        private readonly List<Label> _questionsForTeamB;
+
         private int _dice1Value;
         private int _dice2Value;
+
         public MainForm()
         {
             InitializeComponent();
+            _questionsForTeamA = InitQuestions("A");
+            _questionsForTeamB = InitQuestions("B");
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
             pictureBox1.Image = Image.FromFile(_dicePicture[1]);
             pictureBox2.Image = Image.FromFile(_dicePicture[1]);
+            panel2.Controls.AddRange(_questionsForTeamA.ToArray());
+            panel3.Controls.AddRange(_questionsForTeamB.ToArray());
         }
 
         private async void btnRollDice_Click(object sender, EventArgs e)
@@ -69,6 +69,61 @@ namespace DangerousRaceGame
             lblDice1.Text = string.Empty;
             lblDice2.Text = string.Empty;
             lblTotal.Text = string.Empty;
+        }
+
+        private List<Label> InitQuestions(string teamName)
+        {
+            var questions = new List<Label>();
+            int x = 90, y = 20;
+
+            for (int i = 1; i <= 11; i++)
+            {
+                var location = new Point(x * i, y);
+                var question = new Label
+                {
+                    BackColor =
+                        System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))),
+                            ((int)(((byte)(192))))),
+                    BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle,
+                    Name = teamName + i.ToString(),
+                    Size = new System.Drawing.Size(67, 45),
+                    Text = i.ToString(),
+                    TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                    Cursor = Cursors.Hand,
+                    Location = location
+                };
+
+                if (i == 11)
+                {
+                    question.Location = new Point(x * (i - 1), y + 70);
+                }
+
+                questions.Add(question);
+            }
+
+            int index = 1;
+            for (int i = 21; i >= 12; i--)
+            {
+                var location = new Point(x * index, y + 140);
+                var question = new Label
+                {
+                    BackColor =
+                        System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))),
+                            ((int)(((byte)(192))))),
+                    BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle,
+                    Name = "question" + i.ToString(),
+                    Size = new System.Drawing.Size(67, 45),
+                    Text = i.ToString(),
+                    TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                    Cursor = Cursors.Hand,
+                    Location = location
+                };
+
+                questions.Add(question);
+                index++;
+            }
+
+            return questions;
         }
     }
 }
