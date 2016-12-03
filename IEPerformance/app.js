c85608781fe1d3a4537e77f29bfc2b9ea4fe2743
Tien Tran <Tien Tran>
2016-12-03 16:36:01 +0700

    #0000 - Add css performance example

---
+++ IEPerformance/app.js
@@ -0,0 +1,13 @@
+(function(){
+    var tableBody = document.getElementById("data");
+
+    var count = 6002;
+    var html = [];
+
+    for(var i = 0; i < count; i++){
+        var row = "<tr><td>Data</td><td>Data</td><td>Data</td></tr>";
+        html.push(row);
+    }
+
+    tableBody.innerHTML += html.join();
+})();
